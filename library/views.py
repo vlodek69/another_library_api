6ff710bf700484bd1289607c7ed2157ea5778df3
Vlad Larin <lordgadyka71@gmail.com>
2024-10-26 17:29:44 +0300

    Implement filters using DjangoFilterBackend

--- library/views.py
+++ library/views.py
@@ -1,3 +1,4 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 
 from library.models import Book
@@ -7,4 +8,5 @@ from library.serializers import BookSerializer
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-
+    filter_backends = [DjangoFilterBackend]
+    filter_fields = ["author", "publish_date", "language"]
