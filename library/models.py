b56ff7ab6fed034dff1940e97c9d1a58787e28ec
Vlad Larin <lordgadyka71@gmail.com>
2024-10-26 15:44:26 +0300

    Add Book model in models.py

--- library/models.py
+++ library/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    publish_date = models.DateField(blank=True, null=True)
+    isbn = models.CharField(max_length=16, unique=True)
+    pages = models.IntegerField(blank=True, null=True)
+    cover = models.URLField(max_length=511, blank=True, null=True)
+    language = models.CharField(max_length=255)
