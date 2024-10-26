baecad072f455ef0eba9671440b680a46d6ae935
Vlad Larin <lordgadyka71@gmail.com>
2024-10-26 19:48:51 +0300

    Update to_representation method of BookSerializer
    To enforce `null` in response for empty URL field

--- library/serializers.py
+++ library/serializers.py
@@ -6,4 +6,11 @@ from library.models import Book
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = '__all__'
+        fields = "__all__"
+
+    def to_representation(self, instance):
+        """Enforce `null` if `cover` URLField is empty"""
+        representation = super().to_representation(instance)
+        if representation.get("cover") == "":
+            representation["cover"] = None
+        return representation
