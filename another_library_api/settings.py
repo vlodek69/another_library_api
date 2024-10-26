af59a11e8781ce9a9d683e1f2c4ad7c8a45a1433
Vlad Larin <lordgadyka71@gmail.com>
2024-10-26 15:46:07 +0300

    Add postgresql configs in settings.py

--- another_library_api/settings.py
+++ another_library_api/settings.py
@@ -82,8 +82,11 @@ WSGI_APPLICATION = "another_library_api.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("POSTGRES_DB"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "HOST": os.environ.get("POSTGRES_HOST"),
     }
 }
 
