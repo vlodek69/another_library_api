6a916883339aaeb233d4909e5db0b92afc3940fb
Vlad Larin <lordgadyka71@gmail.com>
2024-10-26 13:44:15 +0300

    add ALLOWED_HOSTS and INTERNAL_IPS in settings.py

--- another_library_api/settings.py
+++ another_library_api/settings.py
@@ -26,7 +26,11 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
 
 
 # Application definition
