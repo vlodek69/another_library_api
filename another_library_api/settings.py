1b252d624d542cacc2f82d56c08bc912a2f02645
Vlad Larin <lordgadyka71@gmail.com>
2024-10-26 13:38:08 +0300

    install drf

--- another_library_api/settings.py
+++ another_library_api/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'library.apps.LibraryConfig',
+    "rest_framework",
+    "library",
 ]
 
 MIDDLEWARE = [
@@ -56,8 +57,7 @@ ROOT_URLCONF = "another_library_api.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / 'templates']
-        ,
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
